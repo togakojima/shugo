9ad33b0502b108fd0b4dc6807afbec55f83ef9ae
togakojima <togakojima1999@gmail.com>
2024-06-29 21:33:47 +0900

    メッセージ機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
 
   has_many :room_users, dependent: :destroy
   has_many :rooms, through: :room_users, dependent: :destroy
+  has_many :messages, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     member do
       post :join
     end
+    resources :messages, only: [:index, :create]
   end
 end
