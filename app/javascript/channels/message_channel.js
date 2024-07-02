e10a5d2347374f9a3b3048f568b8c239c8eea8ba
togakojima <togakojima1999@gmail.com>
2024-07-02 17:58:34 +0900

    コード整理

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -17,47 +17,44 @@ if(location.pathname.match(/\/rooms\/\d/)){
 
     received(data) {
       console.log('Received data:', data);
-  console.log('current_user_id:', data.current_user_id);
-  console.log('message user id:', data.user.id);
-  console.log(data.image_url);
+      console.log('current_user_id:', data.current_user_id);
+      console.log('message user id:', data.user.id);
+      console.log(data.image_url);
 
-  const date = new Date(data.message.created_at);
+      const date = new Date(data.message.created_at);
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const day = date.getDate().toString().padStart(2, '0');
+      const hour = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+      const formattedDate = `${year}/${month}/${day} ${hour}:${minutes}`
 
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hour = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
+      const isCurrentUser = parseInt(data.current_user_id) === parseInt(data.user.id);
+      console.log('isCurrentUser:', isCurrentUser);
 
-  const formattedDate = `${year}/${month}/${day} ${hour}:${minutes}`
+      const html = `
+        <div class="message_${isCurrentUser ? 'me' : 'other'}">
+          <div class="upper-message">
+            <div class="message-user${isCurrentUser ? '' : '_other'}">
+              ${ data.user.nickname }
+            </div>
+          </div>
+          <div class="lower-message">
+            <div class="message-content">
+              ${ data.message.content }
+            </div>
+            ${data.image_url ? `<div class="message-images"><img src="${data.image_url}" class="message-image" /></div>` : ''}
+            <div class="message-date">
+              ${ formattedDate }
+            </div>
+          </div>
+        </div>`;
 
-  const isCurrentUser = parseInt(data.current_user_id) === parseInt(data.user.id);
-  console.log('isCurrentUser:', isCurrentUser);
-
-  const html = `
-    <div class="message_${isCurrentUser ? 'me' : 'other'}">
-      <div class="upper-message">
-        <div class="message-user${isCurrentUser ? '' : '_other'}">
-        ${ data.user.nickname }
-      </div>
-    </div>
-    <div class="lower-message">
-      <div class="message-content">
-        ${ data.message.content }
-      </div>
-      ${data.image_url ? `<div class="message-images"><img src="${data.image_url}" class="message-image" /></div>` : ''}
-      <div class="message-date">
-        ${ formattedDate }
-      </div>
-    </div>
-  </div>`;
-
-    const messages = document.getElementById("comments")
-    messages.insertAdjacentHTML('beforeend', html)
-    const messageForm = document.getElementById("message-form")
-    messageForm.reset();
-
-    messages.scrollTop = messages.scrollHeight;
+      const messages = document.getElementById("comments")
+      messages.insertAdjacentHTML('beforeend', html)
+      const messageForm = document.getElementById("message-form")
+      messageForm.reset();
+      messages.scrollTop = messages.scrollHeight;
     }
   })
 }
